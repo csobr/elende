8f568dba59f09893f97ea78faa3dce9da878059a
Siham Hadi <siham.hadi@live.se>
2022-09-08 14:16:01 +0200

    Update bid area store value

--- src/lib/store.ts
+++ src/lib/store.ts
@@ -2,7 +2,7 @@ import { writable, derived } from 'svelte/store';
 import data from './data.json';
 export const result = writable(data);
 export const contracts = writable([]);
-export const bidArea = writable([1, 2, 3, 4]);
+export const bidArea = writable(['']);
 export const price = writable('');
 
 export const suppliers = derived(result, ($result) => {
