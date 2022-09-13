5c09c1980990d271596115ffa003d304e694becb
Siham Hadi <siham.hadi@live.se>
2022-09-13 13:07:51 +0200

    Update getAllData function

--- src/lib/EIData.ts
+++ src/lib/EIData.ts
@@ -1,17 +1,17 @@
 const date: Date = new Date();
 const fullDate = `${date.getFullYear()}-0${date.getMonth()}-15`;
-export const url = `${import.meta.env.VITE_OG_URL}=${fullDate}`;
+export const url = `${import.meta.env.VITE_HUS_URL}=${fullDate}`;
 
 export const getAllData = async (url: RequestInfo | URL) => {
-  const results: Object[] = [];
+  let result: Object[] = [];
   while (url) {
     const response = await fetch(url);
     if (!response.ok) {
       throw new Error('HTTP error ' + response.status);
     }
     const { results, next } = await response.json();
-    results.push(...results);
+    result.push(...results);
     url = next;
   }
-  return results;
+  return result;
 };
